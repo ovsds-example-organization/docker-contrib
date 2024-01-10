8f722ea43d1ea647c686ad65034b63c55995a7af
ovsds <ovsds@yandex-team.ru>
2024-01-10 11:06:25 +0100

    fix(ubuntu-ci): update node version Solved (CVE-2022-25881)[https://avd.aquasec.com/nvd/cve-2022-25881]

--- src/ubuntu-ci/jammy/scripts/300-node.sh
+++ src/ubuntu-ci/jammy/scripts/300-node.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo 'Installing nvm...'
 
-export NVM_VERSION=0.39.1
+export NVM_VERSION=0.39.7
 export NVM_DIR="${HOME}/.nvm"
 
 curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v${NVM_VERSION}/install.sh" | bash
@@ -12,17 +12,15 @@ source "${NVM_DIR}/nvm.sh"
 
 echo 'Installing node...'
 
-export NODE_VERSION=16.15.1
+export NODE_VERSION=v21.5.0
 
 nvm install ${NODE_VERSION}
 nvm alias default ${NODE_VERSION}
 nvm use default
 nvm cache clear
 
-npm config --location=global set user root
 npm config --location=global set shell bash
 npm config --location=global set fund false
 npm config --location=global set audit false
 npm config --location=global set fetch-retries 3
-npm config --location=global set unsafe-perm true
 npm config --location=global set update-notifier false
